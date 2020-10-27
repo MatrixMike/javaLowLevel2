807d12ee62bf9ab31bcf6fdaf9bc0602ac88f48e
Mike on Tecra <matroxmike@icloud.com>
2020-10-27 15:01:41 +1100

    inspect with gitk

--- intersection2.java
+++ intersection2.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 
 public class intersection2 {
     public static void main(String[] args) {
-        TreeSet<String> evenNumbers = new TreeSet<>();   // was new CustomComparator()
+        TreeSet<String> evenNumbers = new TreeSet<>(new CustomComparator());
 
         evenNumbers.add("Peach");        
         evenNumbers.add("Orange");
