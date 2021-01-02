e8ff7a6552c4fecf88cbff0f85a093fb41de8ef5
Mike on Tecra <matroxmike@icloud.com>
2021-01-02 13:44:22 +1100

    more ranges

--- MaritimeFreq/MaritimeFreq.java
+++ MaritimeFreq/MaritimeFreq.java
@@ -29,6 +29,51 @@ public class MaritimeFreq {
         LandTx = LandTxArg;
         Channel = ChannelArg;
     }
+    public static	void Add60range()
+	{
+/*
+    public static List<GearRatios> createGearRatios() {
+        List<GearRatios> RatiosList = new ArrayList<>();
+
+
+*/
+		
+		System.out.println("6X range start");
+		Stream.iterate(156025, n -> n + m7spacing).limit(9)  // setup for range for 6X
+		.forEach(System.out::println);
+		System.out.println("6X range end");
+	}    
+    
+    public static void Add70range()
+	{	
+		System.out.println("7X range start");
+		Stream.iterate(156525, n -> n + m7spacing).limit(10)  // setup for range for 7X
+		.forEach(System.out::println);
+		System.out.println("7X range end");
+	}
+	    public static void Add80range()
+	{	
+		System.out.println("8X range start");
+		Stream.iterate(157025, n -> n + m7spacing).limit(9)  // setup for range for 7X
+		.forEach(System.out::println);
+		System.out.println("8X range end");
+	}
+	
+	public static void Add00range()
+	{	
+		System.out.println("0X range start");
+		Stream.iterate(156050, n -> n + m7spacing).limit(19)  // setup for range for 7X
+		.forEach(System.out::println);
+		System.out.println("0X range end");
+	}
+	public static void Repeaters_range()
+	{	
+		System.out.println("Repeaters start");
+		Stream.iterate(157025, n -> n + mspacing).limit(5)  // setup for range for 7X
+		.forEach(System.out::println);
+		System.out.println("Repeaters end");
+	}	
+	
     public static List<MaritimeFreq> createGearRatios() {
 		List<MaritimeFreq> FrequencyList = new ArrayList<>();
 	return FrequencyList;	
@@ -44,10 +89,14 @@ Add channel 6X - spacing 50 kHz - start 156.025  	160.625       60
 	
 	private static final int tspacing = 250;	
 	private static final int mspacing = 25;
+		private static final int m7spacing = 50;
 		
 	public static void main (String[] args) {
-		
-		
+		Add00range();
+		Add80range();
+		Repeaters_range();
+		Add70range();	
+		Add60range();	
 /*
 	Stream.iterate(4764250, n -> n + tspacing).limit(35)
 	.forEach(System.out::println);
@@ -55,9 +104,19 @@ Add channel 6X - spacing 50 kHz - start 156.025  	160.625       60
 */
 
 // frequency numbers below are kHz 
+/*
 	Stream.iterate(156000, n -> n + mspacing).limit(250)  // practical start 156025
 	.forEach(System.out::println);	
+
+
+*/
+
+
 	
 	}
+
+
+
+	
 }
 
