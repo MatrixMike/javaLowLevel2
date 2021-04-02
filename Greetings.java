0ad425053cb2f24521069d791b6f5a8d5eba19e3
MatrixMike <matroxmike@icloud.com>
2021-04-02 13:28:17 +1100

    printing formats 2

--- Greetings.java
+++ Greetings.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class Greetings {
 
     public static void main(String[] args) {
@@ -7,12 +7,16 @@ public class Greetings {
         Scanner newscanner = new Scanner(System.in);
 
         String ask = getString(newscanner, "Please enter your first name: ");
-
+// Order %d: %.2f %s sent
         // String ask2 = getString(newscanner, "Please enter your last name: ");
 
         // String ask3 = getString(newscanner, "Please enter your year of birth:
         // ");
-
+        System.out.println("ask is " + ask);
+  //      System.out.format("Order %d: %.2f  sent " , ask , 2.3 );
+ // TODO        String.format(Locale.US,
+  String.format(Locale.US,"Order %d: %.2f %s sent ", 19 , 3.2 , ask ) ;
+  System.out.println(String.format(Locale.US,"Order %d: %.2f %s sent ", 19 , 3.2 , ask ) );
     }
 
     public static String getString(Scanner newscanner, String ask) {
