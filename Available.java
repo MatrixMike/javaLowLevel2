12b5b222ebbf7cd8ebace253508754de0bbf5f38
MatrixMike <matroxmike@icloud.com>
2021-04-05 23:00:38 +1000

    Locale.getDefault()

--- Available.java
+++ Available.java
@@ -1,11 +1,16 @@
 import java.util.*;
 import java.text.*;
+import java.util.Locale;
 
 public class Available {
     static public void main(String[] args) {
+		int mLevel = 19;
         Locale list[] = DateFormat.getAvailableLocales();
         for (Locale aLocale : list) {
             System.out.println(aLocale.toString());
         }
+        
+        System.out.println(String.format( Locale.getDefault(), "Level %d", ++mLevel));
+        System.out.println(String.format( Locale.getDefault(), "Math.pi %f", 3.14));
     }
 }
